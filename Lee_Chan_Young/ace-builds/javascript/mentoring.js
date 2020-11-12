8a6a83f3bcd4878c4fa34c6cb613b6c36cd72997
ChanyoungLee <cammm988@naver.com>
2020-11-13 00:33:42 +0900

    mentoring with source editor submission button added

--- Lee_Chan_Young/ace-builds/javascript/mentoring.js
+++ Lee_Chan_Young/ace-builds/javascript/mentoring.js
@@ -2,6 +2,7 @@
 var modal = document.getElementById('myModal');
 var modal2 = document.getElementById('myModal2');
 
+var submission = document.getElementById("submission");
 // Get the button that opens the modal
 var btn = document.getElementById("mentoring");
 
@@ -20,4 +21,9 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
+}
+
+
+submission.onclick = function(){
+    alert("제출되었습니다.");
 }
