d65c32d09f1459b5c6c83c3f047a06859cce2e0e
ChanyoungLee <cammm988@naver.com>
2020-11-12 22:05:16 +0900

    mentoring modal popup two separated boxes

--- Lee_Chan_Young/ace-builds/javascript/mentoring.js
+++ Lee_Chan_Young/ace-builds/javascript/mentoring.js
@@ -1,4 +1,5 @@
 var modal = document.getElementById('myModal');
+var modal2 = document.getElementById('myModal2');
 
 // Get the button that opens the modal
 var btn = document.getElementById("mentoring");
@@ -14,11 +15,4 @@ btn.onclick = function() {
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 }
