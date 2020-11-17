3ddc5daa2b2cb71ed960b0d1482ee458a92c0665
ChanyoungLee <cammm988@naver.com>
2020-11-17 20:52:28 +0900

    Stackoverflow css style added

--- ace-builds/javascript/stackoverflow.js
+++ ace-builds/javascript/stackoverflow.js
@@ -5,13 +5,16 @@ document.onmouseup = function() {
 }
 
 function stackoverflow() {
+
     if (selectionText.toString() == "" ) {
         alert("원하는 내용을 하이라이트 한 후 버튼을 눌러주십시오")
     }
-    else {
-        alert(selectionText) + "\n코드를 검색합니다";
-        // document.getElementsByTagName("object")[0].setAttribute("data", "https://faq.exchange/#"+selectionText.toString());
-        document.getElementById("stackoverflow").innerHTML = "<object type=\"text/html\" data=\"https://faq.exchange/#"+selectionText.toString() + " \"width=\"800px\" height=\"600px\" style=\"overflow:auto;border:3px ridge blue\"> </object>"
-
+    else if (selectionText.toString().length >=40) {
+        alert("하이라이트 된 코드가 너무 깁니다. 다시 시도해주세요.")
     }
+    else {
+        alert(selectionText.toString() + "\n코드를 검색합니다");
+        // document.getElementsByTagName("object")[0].setAttribute("data", "https://faq.exchange/#"+selectionText.toString());
+        document.getElementById("stackoverflow").innerHTML = "<object type=\"text/html\" data=\"https://faq.exchange/#"+selectionText.toString() + " \"width=\"800px\" height=\"600px\" style=\"overflow:auto;\"> </object>"
+ㄴ    }
 }
