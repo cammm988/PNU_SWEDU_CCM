7a4caa964562c106b6efa261d85056d779693444
ChanyoungLee <cammm988@naver.com>
2020-11-17 20:16:09 +0900

    Stackoverflow work revised

--- ace-builds/javascript/stackoverflow.js
+++ ace-builds/javascript/stackoverflow.js
@@ -2,20 +2,16 @@ var selectionText = "";
 
 document.onmouseup = function() {
         selectionText = document.getSelection();
-
 }
-
 
 function stackoverflow() {
-    // document.getElementsByTagName("object")[0].setAttribute("data", "https://faq.exchange/"+selectionText);
-    location.href="https://faq.exchange/+python"
+    if (selectionText.toString() == "" ) {
+        alert("원하는 내용을 하이라이트 한 후 버튼을 눌러주십시오")
+    }
+    else {
+        alert(selectionText) + "\n코드를 검색합니다";
+        // document.getElementsByTagName("object")[0].setAttribute("data", "https://faq.exchange/#"+selectionText.toString());
+        document.getElementById("stackoverflow").innerHTML = "<object type=\"text/html\" data=\"https://faq.exchange/#"+selectionText.toString() + " \"width=\"800px\" height=\"600px\" style=\"overflow:auto;border:3px ridge blue\"> </object>"
 
-    // document.getElementById("stackoverflow").innerHTML =
-    alert(selectionText);
-
-}
-
-function updateDiv()
-{
-    $( "#here" ).load(window.location.href + " #here" );
+    }
 }
